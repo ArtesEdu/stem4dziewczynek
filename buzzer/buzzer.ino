f6c1b9c563c06ba19c394a2d115d5318d0f57f76
Santiago Zarate <santiago@zarate.co>
2023-07-25 08:45:39 +0200

    first working prototype

--- buzzer/buzzer.ino
+++ buzzer/buzzer.ino
@@ -7,27 +7,64 @@
 // https://maker.pro/arduino/projects/buzzer
 /************************************/
 int buzzer = 12;  //the pin of the active buzzer
+
+#include <U8g2lib.h>
+#include <Wire.h>
+
+U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
+
 void setup() {
   // start serial port at 9600 bps and wait for port to open:
   Serial.begin(9600);
-  while (!Serial) {
-    ;  // wait for serial port to connect. Needed for native USB port only
-  }
-  pinMode(buzzer, OUTPUT);  //initialize the buzzer pin as an output
   establishContact();
+  // while (!Serial) {
+  //   ;  // wait for serial port to connect. Needed for native USB port only
+  // }
+
+  u8g2.begin();
+
+
+  pinMode(buzzer, OUTPUT);  //initialize the buzzer pin as an output
+
 }
 
 void establishContact() { 
-  while (Serial.available() <= 0) {
-    Serial.println("0,0,0");  // send an initial string
+int incomingByte = 0;
+  Serial.println("Proszę czekać");  // send an initial string
+  while (!Serial) {
+    delay(100);
+    Serial.println("Proszę czekać");  // send an initial string
+        // read the incoming byte:
     delay(300);
   }
+  Serial.println("Witaj w arduino, hakerze!");  // send an initial string
+  delay(100);
 }
 
 void loop() {
-  //digitalWrite(buzzer,HIGH);
-  //digitalWrite(buzzer,LOW);
-  Serial.print("stem4dziewczynek");
-  Serial.println("stem4dziewczynek");
+  // u8g2.clearBuffer();
+  // delay(100);
+  // u8g2.setFont(u8g2_font_logisoso16_tr);
+  // delay(100);
+  // u8g2.drawStr(5,35,"ideasss");
+  // delay(100);
+  // u8g2.sendBuffer();
+  // delay(10000);
+  // digitalWrite(buzzer,HIGH);
+  // digitalWrite(buzzer,LOW);
+  // delay(100);
+
+  Serial.println("stem4dziewczynek!");
+  delay(100);
+  Serial.println("hello Anna, Ania!");
+  delay(50);
+
+    u8g2.clearBuffer();                       // Clean the device buffer
+    u8g2.setFont(u8g2_font_logisoso16_tr);    // Select the font
+    u8g2.drawStr(30,35,"stem4dziewczynek");   // Ask the display to write a string at (X,Y) coordinates on the screen 
+    u8g2.sendBuffer();                        // Send 
+    delay(1000);
+    u8g2.clearBuffer(); 
+  
   exit(0);
 }
---
+++ display/display.ino
@@ -0,0 +1,17 @@
+#include <U8g2lib.h>
+#include <Wire.h>
+#define time_delay 2000
+U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
+
+void setup() {
+  // put your setup code here, to run once:
+  u8g2.begin();
+}
+
+void loop() {
+    u8g2.clearBuffer();                    // Vaciamos el buffer del dispositivo
+    u8g2.setFont(u8g2_font_logisoso16_tr);    // Seleccionamos la fuente
+    u8g2.drawStr(5,35,"ideasss");    // Escribimos en el buffer el texto con la posición (X,Y) en la pantalla
+    u8g2.sendBuffer();                    // Enviamos el buffer a nuestra pantalla
+    delay(1000);  
+}
